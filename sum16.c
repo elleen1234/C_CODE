3dd95d12a558f4482140bde4aa5dc9e739cfb0ab
unknown <nayak.pravasini@gmail.com>
2024-03-16 10:30:54 +0530

    adding files for initial setup

---
+++ sum16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("enter the two numbers");
+	scanf("%d%d",&a,&b);
+	int sum=a+b;
+	printf("sum is %d\n",sum);
+	return 0;
+}
+	
